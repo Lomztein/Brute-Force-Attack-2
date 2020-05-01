d3137199c7823c27b1fea597565a3ece4d8da4fd
Marcus L. Jensen <lomztein2@gmail.com>
2020-05-01 04:44:31 +0200

    Added source files to be implemented

---
+++ Assets/Source/Turrets/Targeters/TurretRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
