6639bb3a3097f60c39984d44965a1d5d8435cc61
Marcus L. Jensen <lomztein2@gmail.com>
2020-05-08 04:15:26 +0200

    Final wave now slowly increase spawnrate

---
+++ Assets/Source/Enemies/Waves/Spawners/EndlessSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lomztein.BFA2.Enemies.Waves.Spawners
+{
+    public class EndlessSpawner : MonoBehaviour, ISpawner
+    {
+        public GameObject SpawnerPrefab;
+
+        public float StepUpDelay;
+        public float StepUpSpawnFrequencyMult;
+
+        public event Action<GameObject> OnSpawn;
+
+        private int _amount;
+        private float _delay;
+        private GameObject _prefab;
+
+        public void Spawn(int amount, float delay, GameObject prefab)
+        {
+            _amount = amount;
+            _delay = delay;
+            _prefab = prefab;
+
+            SpawnSpawner(amount, delay, prefab);
+            InvokeRepeating("StepUp", StepUpDelay, StepUpDelay);
+        }
+
+        private void StepUp ()
+        {
+            float delay = _delay / StepUpSpawnFrequencyMult;
+            SpawnSpawner(_amount, delay, _prefab);
+        }
+
+        private void SpawnSpawner (int amount, float delay, GameObject prefab)
+        {
+            GameObject obj = Instantiate(SpawnerPrefab);
+            ISpawner spawner = obj.GetComponent<ISpawner>();
+            spawner.OnSpawn += OnNestedSpawn;
+            spawner.Spawn(amount, delay, prefab);
+        }
+
+        private void OnNestedSpawn(GameObject obj)
+        {
+            OnSpawn?.Invoke(obj);
+        }
+    }
+}
