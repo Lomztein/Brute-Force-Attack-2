8dee232086d61e47ade75716c95b8c6da5248c1a
Marcus L. Jensen <lomztein2@gmail.com>
2020-09-03 16:05:13 +0200

    Fixed issue where dynamically generated icons would overlap

--- Assets/Source/UI/Iconography.cs
+++ Assets/Source/UI/Iconography.cs
@@ -11,6 +11,10 @@ namespace Lomztein.BFA2.UI
     public class Iconography : MonoBehaviour
     {
         private static Iconography _instance;
+
+        private const float DIST_FROM_WORLD = 1024f;
+        private static int _index;
+
         private static Camera Camera => _instance.RenderCamera;
         public const int RENDER_SIZE = 128;
 
@@ -20,6 +24,12 @@ namespace Lomztein.BFA2.UI
         {
             _instance = this;
             gameObject.SetActive(false);
+        }
+
+        private static Vector3 GetPosition ()
+        {
+            Quaternion rot = Quaternion.Euler(0f, 0f, ++_index);
+            return rot * (Vector3.one * DIST_FROM_WORLD);
         }
 
         public static GameObject InstantiateModel(GameObject source)
@@ -37,43 +47,45 @@ namespace Lomztein.BFA2.UI
         public static Texture2D GenerateIcon(GameObject obj)
         {
             _instance.gameObject.SetActive(true);
+            _instance.transform.position = GetPosition();
 
             Camera.enabled = true;
             Camera.aspect = 1f;
 
             GameObject model = InstantiateModel(obj);
+
             model.transform.position = _instance.transform.position;
             model.SetActive(true);
 
-            RenderTexture renderTexture = new RenderTexture(RENDER_SIZE, RENDER_SIZE, 24);
-            renderTexture.Create();
+            RenderTexture renderTexture = RenderTexture.GetTemporary(RENDER_SIZE, RENDER_SIZE, 24);
 
             Bounds bounds = GetObjectBounds(model);
 
             float camSize = Mathf.Abs(bounds.extents.y);
             Camera.orthographicSize = camSize;
 
+            RenderTexture.active = renderTexture;
+            GL.Clear(true, true, Color.clear);
+
             Camera.targetTexture = renderTexture;
             Camera.transform.position = _instance.transform.position + bounds.center + Camera.transform.forward * -100f;
             Camera.transform.LookAt(_instance.transform);
             Camera.Render();
 
-            RenderTexture.active = renderTexture;
-
             Texture2D texture = new Texture2D(RENDER_SIZE, RENDER_SIZE, TextureFormat.ARGB32, false);
             texture.ReadPixels(new Rect(0f, 0f, RENDER_SIZE, RENDER_SIZE), 0, 0);
             texture.Apply();
 
-            Camera.targetTexture = null;
             RenderTexture.active = null;
 
-            Destroy(renderTexture);
+            RenderTexture.ReleaseTemporary(renderTexture);
+            Camera.targetTexture = null;
 
             Camera.enabled = false;
             _instance.gameObject.SetActive(false);
 
             model.SetActive(false);
-            Destroy(model);
+            DestroyImmediate(model);
             return texture;
         }
 
